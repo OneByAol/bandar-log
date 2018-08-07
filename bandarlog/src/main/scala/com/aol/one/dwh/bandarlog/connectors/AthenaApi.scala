598022d27c90d7bf58dec2c0411fd9a22ac906cd
Maria Rabyk <mariia.rabyk@gmail.com>
2018-08-07 09:20:51 +0300

    Added method for fetching all results (String) from ResultSet and extended LogTrait

--- bandarlog/src/main/scala/com/aol/one/dwh/bandarlog/connectors/AthenaApi.scala
+++ bandarlog/src/main/scala/com/aol/one/dwh/bandarlog/connectors/AthenaApi.scala
@@ -1,8 +1,10 @@
 package com.aol.one.dwh.bandarlog.connectors
 
-import java.sql.{Connection, DriverManager, Statement}
+import java.sql.{Connection, DriverManager, ResultSet, Statement}
 
-class AthenaApi {
+import com.aol.one.dwh.infra.util.LogTrait
+
+class AthenaApi extends LogTrait{
 
   private val DB_DRIVER: String = "com.simba.athena.jdbc.Driver"
   private val DB_CONNECTION: String = "jdbc:awsathena://AwsRegion=eu-central-1;" +
@@ -12,5 +14,13 @@ class AthenaApi {
   Class.forName(DB_DRIVER)
   val connection: Connection = DriverManager.getConnection(DB_CONNECTION)
   val stmt: Statement = connection.createStatement()
+
+  private def fetchAll(rs: ResultSet): List[String] = {
+    Iterator
+      .continually(rs.next)
+      .takeWhile(identity)
+      .map { _ => rs.getString(1) }
+      .toList
+  }
 
 }
