2e301fd777ee46e78380d8ad95175c4ade1bd010
Maria Rabyk <mariia.rabyk@gmail.com>
2018-08-07 15:13:35 +0300

    Added method for getting max batchId using AWS Glue client, removed rest of methods

--- bandarlog/src/main/scala/com/aol/one/dwh/bandarlog/connectors/AthenaApi.scala
+++ bandarlog/src/main/scala/com/aol/one/dwh/bandarlog/connectors/AthenaApi.scala
@@ -1,11 +1,18 @@
 package com.aol.one.dwh.bandarlog.connectors
 
 import java.sql._
-import java.text.SimpleDateFormat
-import java.time.Duration
 
 import com.aol.one.dwh.infra.util.LogTrait
+import com.simba.athena.amazonaws.services.glue.AWSGlueClient
+import com.simba.athena.amazonaws.services.glue.model.{GetPartitionsRequest, Partition}
 
+import scala.collection.JavaConversions._
+
+/**
+  * Athena Connector
+  *
+  * Provides access to the metadata table that represents your data, including its schema
+  */
 class AthenaApi extends LogTrait {
 
   private val DB_DRIVER: String = "com.simba.athena.jdbc.Driver"
@@ -19,98 +26,18 @@ class AthenaApi extends LogTrait {
 
   val db_name = "Athena"
 
-  private def fetchAll(rs: ResultSet): List[String] = {
-    Iterator
-      .continually(rs.next)
-      .takeWhile(identity)
-      .map { _ => rs.getString(1) }
-      .toList
+  private def getPartitions(table_name: String): List[Partition] = {
+    val glue = AWSGlueClient.builder().withRegion("eu-central-1").build()
+    val request = new GetPartitionsRequest
+    request.setDatabaseName(db_name)
+    request.setTableName(table_name)
+    glue.getPartitions(request).getPartitions.toList
   }
 
-  def get_table_location(table_name: String): Option[String] = {
-    val stmt = connection.createStatement()
-    try {
-      logger.info(s"Request table $table_name definition to extract location.")
-      val sql = s"SHOW CREATE TABLE $db_name.$table_name"
-      logger.info(s"Running query:[$sql].")
-      val rs: ResultSet = stmt.executeQuery(sql)
-      val location = fetchAll(rs).filter(x => x.contains("s3://"))
-      rs.close()
-      Some(location.head.trim.replace("\'", ""))
-    } catch {
-      case ex: SQLException =>
-        logger.error(s"Table $table_name was not found.")
-        None
-    } finally {
-      stmt.close()
-    }
+  def getMaxBatchIdPartition(table_name: String): Long = {
+    val partitions = getPartitions(table_name).sortWith(_.getValues.get(0) > _.getValues.get(0))
+    val maxBatchId = partitions.head.getValues().get(0).toLong
+    logger.info(s"Max batch_id for table $table_name is $maxBatchId")
+    maxBatchId
   }
-
-  def set_table_location(table_name: String, new_location: String): Unit = {
-    val stmt = connection.createStatement()
-    try {
-      logger.info(s"Set new table $table_name location $new_location")
-      val sql = s"ALTER TABLE $db_name.$table_name SET LOCATION '$new_location'"
-      logger.info(s"Running query:[$sql]")
-      stmt.execute(sql)
-    } catch {
-      case ex: SQLException =>
-        logger.error(s"Cannot set new location, error: ${ex.getMessage}.")
-    } finally {
-      stmt.close()
-    }
-  }
-
-  def recover_all_partitions(table_name: String): Unit = {
-    val stmt = connection.createStatement()
-    try {
-      val sql = s"MSCK REPAIR TABLE $db_name.$table_name"
-      logger.info(s"Running query:[$sql].")
-      stmt.execute(sql)
-    } catch {
-      case ex: SQLException =>
-        logger.error(s"Cannot load partitions, error: ${ex.getMessage}.")
-    } finally {
-      stmt.close()
-    }
-  }
-
-  def check_if_table_exists(table_name: String): Option[Boolean] = {
-    val stmt = connection.createStatement()
-    try {
-      logger.info(s"Check if table $table_name exists in Athena.")
-      val sql = s"SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = '$table_name'"
-      logger.info(s"Running query:[$sql].")
-      val rs: ResultSet = stmt.executeQuery(sql)
-      val check = rs.next
-      rs.close()
-      Some(check)
-    } catch {
-      case ex: SQLException =>
-        logger.error(s"Table $table_name does not exist.")
-        None
-    } finally {
-      stmt.close()
-    }
-  }
-
-  def clean_table(table_name: String, table_window_in_millis: Long): Unit = {
-    val stmt = connection.createStatement()
-    try {
-      val deltaTime: Duration = Duration.ofDays(1).plusMillis(table_window_in_millis)
-      val timediff = System.currentTimeMillis() - deltaTime.toMillis
-      val dateFormat = new SimpleDateFormat("YYYY-mm-dd")
-      val date = dateFormat.format(timediff)
-      val sql = s"ALTER TABLE $db_name.$table_name DROP PARTITION(date='$date')"
-      logger.info(s"Running query:[$sql].")
-      stmt.execute(sql)
-    } catch {
-      case ex: SQLException =>
-        logger.error(s"cannot clean table $table_name, error: ${ex.getMessage}.")
-    } finally {
-      stmt.close()
-    }
-  }
-
-
 }
