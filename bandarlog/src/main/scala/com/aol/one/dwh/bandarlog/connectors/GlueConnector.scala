ddef3b095516ddc14096035d3c3fce78aab8dd5b
Maria Rabyk <mariia.rabyk@gmail.com>
2018-08-07 16:59:28 +0300

    Added Glue connector with method to get max batchId

---
+++ bandarlog/src/main/scala/com/aol/one/dwh/bandarlog/connectors/GlueConnector.scala
@@ -0,0 +1,38 @@
+package com.aol.one.dwh.bandarlog.connectors
+
+import com.aol.one.dwh.infra.aws.BandarlogAWSCredentialsProvider
+import com.aol.one.dwh.infra.config.GlueConfig
+import com.aol.one.dwh.infra.util.LogTrait
+import com.simba.athena.amazonaws.services.glue.AWSGlueClient
+import com.simba.athena.amazonaws.services.glue.model.{GetPartitionsRequest, Partition}
+
+import scala.collection.JavaConversions._
+
+/**
+  * Glue Connector
+  *
+  * Provides access to the metadata table
+  */
+class GlueConnector(config: GlueConfig) extends LogTrait {
+
+  private val credentialProvider = new BandarlogAWSCredentialsProvider(config: GlueConfig)
+  private val glueClient = AWSGlueClient.builder()
+    .withRegion(config.region)
+    .withCredentials(credentialProvider)
+    .build()
+
+  private def getPartitions(table_name: String): List[Partition] = {
+
+    val request = new GetPartitionsRequest
+    request.setDatabaseName(config.database)
+    request.setTableName(table_name)
+    glueClient.getPartitions(request).getPartitions.toList
+  }
+
+  def getMaxBatchId(table_name: String): Long = {
+    val partitions = getPartitions(table_name).sortWith(_.getValues.get(0) > _.getValues.get(0))
+    val maxBatchId = partitions.head.getValues.get(0).toLong
+    logger.info(s"Max batch_id for table $table_name is $maxBatchId")
+    maxBatchId
+  }
+}
