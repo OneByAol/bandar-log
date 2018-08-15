8a17c34526a3209935d44c695427802c74979727
Maria Rabyk <mariia.rabyk@gmail.com>
2018-08-15 17:31:04 +0300

    Updated SqlTimestampProviderTest for GlueConnector

--- bandarlog/src/test/scala/com/aol/one/dwh/bandarlog/providers/SqlTimestampProviderTest.scala
+++ bandarlog/src/test/scala/com/aol/one/dwh/bandarlog/providers/SqlTimestampProviderTest.scala
@@ -8,7 +8,8 @@
 
 package com.aol.one.dwh.bandarlog.providers
 
-import com.aol.one.dwh.bandarlog.connectors.JdbcConnector
+import com.aol.one.dwh.bandarlog.connectors.{GlueConnector, JdbcConnector}
+import com.aol.one.dwh.infra.config.TableColumn
 import com.aol.one.dwh.infra.sql.Query
 import org.mockito.Matchers.any
 import org.mockito.Mockito.when
@@ -19,8 +20,11 @@ import org.scalatest.mock.MockitoSugar
 class SqlTimestampProviderTest extends FunSuite with MockitoSugar {
 
   private val query = mock[Query]
+  private val table = mock[TableColumn]
   private val jdbcConnector = mock[JdbcConnector]
+  private val glueConnector = mock[GlueConnector]
   private val sqlTimestampProvider = new SqlTimestampProvider(jdbcConnector, query)
+  private val glueTimestampProvider = new GlueTimestampProvider(glueConnector, table)
 
   test("check timestamp value by connector and query") {
     val resultTimestamp = Some(1234567890L)
@@ -31,6 +35,15 @@ class SqlTimestampProviderTest extends FunSuite with MockitoSugar {
     assert(result.getValue == resultTimestamp)
   }
 
+  test("check timestamp value by glue connector and table") {
+    val glueTimestamp = 1533709910004L
+    when(glueConnector.getMaxBatchId(any(), any())).thenReturn(glueTimestamp)
+
+    val result = glueTimestampProvider.provide()
+
+    assert(result.getValue == Some(glueTimestamp))
+  }
+
   test("return none if can't get timestamp value") {
     when(jdbcConnector.runQuery(any(classOf[Query]), any())).thenReturn(None)
 
@@ -38,4 +51,13 @@ class SqlTimestampProviderTest extends FunSuite with MockitoSugar {
 
     assert(result.getValue.isEmpty)
   }
+
+  test("return zero if can't get timestamp value in glue connector") {
+    when(glueConnector.getMaxBatchId(any(), any())).thenReturn(0)
+
+    val result = glueTimestampProvider.provide()
+
+    assert(result.getValue == Some(0))
+  }
+
 }
