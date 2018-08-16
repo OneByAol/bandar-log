37f5ae2d12cd9f76fda7935196a3611bc5c70bbd
Maria Rabyk <mariia.rabyk@gmail.com>
2018-08-16 15:11:37 +0300

    Added copyright notice

--- bandarlog/src/main/scala/com/aol/one/dwh/bandarlog/providers/ProviderFactory.scala
+++ bandarlog/src/main/scala/com/aol/one/dwh/bandarlog/providers/ProviderFactory.scala
@@ -1,3 +1,11 @@
+/*
+  ******************************************************************************
+  * Copyright 2018, Oath Inc.
+  * Licensed under the terms of the Apache Version 2.0 license.
+  * See LICENSE file in project root directory for terms.
+  ******************************************************************************
+*/
+
 package com.aol.one.dwh.bandarlog.providers
 
 import com.aol.one.dwh.bandarlog.connectors.{GlueConnector, JdbcConnector}
