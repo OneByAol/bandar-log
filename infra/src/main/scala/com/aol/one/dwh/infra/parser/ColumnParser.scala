cb1417d60af7292a152c90cddb5dfcbb0dc405f8
maria ra <mariia.rabyk@gmail.com>
2019-03-21 17:21:28 +0200

    Edited column parser

--- infra/src/main/scala/com/aol/one/dwh/infra/parser/ColumnParser.scala
+++ infra/src/main/scala/com/aol/one/dwh/infra/parser/ColumnParser.scala
@@ -1,5 +1,7 @@
 package com.aol.one.dwh.infra.parser
 
+import com.aol.one.dwh.infra.config.Partition
+
 import scala.util.parsing.combinator.RegexParsers
 
 
@@ -11,12 +13,11 @@ object ColumnParser extends RegexParsers {
   def column: Parser[String] = "\\w+".r
   def format: Parser[String] = "[\\w\\-\\.\\s\\':,]+".r
 
-  def pair: Parser[(String, Option[String])] = column ~ opt(pairSep) ~ opt(format) ^^ {
-    case c ~ _ ~ None => (c, None)
-    case c ~ _ ~ Some(f) => (c, Some(f))
+  def pair: Parser[Partition] = column ~ pairSep ~ format ^^ {
+    case c ~ _ ~ f => Partition(c, f)
   }
 
-  def parse(in: String): (String, Option[String]) = {
+  def parse(in: String): Partition = {
     parseAll(pair, in) match {
       case Success(result, _) => result
       case NoSuccess(msg, next) =>
@@ -24,7 +25,7 @@ object ColumnParser extends RegexParsers {
     }
   }
 
-  def parseList(in: List[String]): List[(String, Option[String])] = {
+  def parseList(in: List[String]): List[Partition] = {
     in.map(parse)
   }
 }
