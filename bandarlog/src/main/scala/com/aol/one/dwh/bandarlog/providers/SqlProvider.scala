b2c4cfc22cc9dd3bebb30476bc69cc6f215a134e
maria ra <mariia.rabyk@gmail.com>
2019-03-21 17:26:54 +0200

    Edited SqlTimestampProvider, added logic for parsing date values

--- bandarlog/src/main/scala/com/aol/one/dwh/bandarlog/providers/SqlProvider.scala
+++ bandarlog/src/main/scala/com/aol/one/dwh/bandarlog/providers/SqlProvider.scala
@@ -10,15 +10,14 @@ package com.aol.one.dwh.bandarlog.providers
 
 import com.aol.one.dwh.bandarlog.connectors.{GlueConnector, JdbcConnector}
 import com.aol.one.dwh.bandarlog.metrics.{AtomicValue, Value}
-import com.aol.one.dwh.infra.sql.{DateResultHandler, LongValueResultHandler, Query}
-import SqlProvider._
-import com.aol.one.dwh.infra.config.{Partition, TableColumn}
-import com.aol.one.dwh.infra.parser.FormatParser
+import com.aol.one.dwh.bandarlog.providers.SqlProvider._
+import com.aol.one.dwh.infra.config.{Table, TableColumn, TablePartition}
+import com.aol.one.dwh.infra.parser.StringToTimestampParser
+import com.aol.one.dwh.infra.sql.{ListStringResultHandler, LongValueResultHandler, Query}
 
 object SqlProvider {
   type Timestamp = Long
   type TimestampProvider = Provider[Timestamp]
-  type DateProvider =  Provider[String]
 }
 
 /**
@@ -26,71 +25,72 @@ object SqlProvider {
   *
   * Provides timestamp metric by query and appropriate connector
   */
-class SqlTimestampProvider(connector: JdbcConnector, query: Query) extends TimestampProvider {
+class SqlTimestampProvider(connector: JdbcConnector, query: Query, table: Table) extends TimestampProvider {
 
   override def provide(): Value[Timestamp] = {
-    AtomicValue(connector.runQuery(query, new LongValueResultHandler))
+    table match {
+      case _: TableColumn => AtomicValue(connector.runQuery(query, new LongValueResultHandler))
+
+      case _: TablePartition =>
+        val partitions = table.columns
+        val numberOfColumns = partitions.length
+        val format = table.formats.mkString(":")
+
+        val partitionValues = AtomicValue(connector.runQuery(query, new ListStringResultHandler(numberOfColumns)))
+        val result = for {
+          values <- partitionValues.getValue
+          value <- values
+          longValue = StringToTimestampParser.parse(value, format)
+        } yield {
+          longValue
+        }
+        AtomicValue(result.max)
+    }
   }
 }
 
-class SqlDateProvider(connector: JdbcConnector, query: Query, format: String) extends TimestampProvider {
-  override def provide(): Value[Timestamp] = {
-    val maxPartitionValue = AtomicValue(connector.runQuery(query, new DateResultHandler))
+  /**
+    * Glue Timestamp Provider
+    *
+    * Provides timestamp metric by table, partition column and appropriate connector
+    */
+  class GlueTimestampProvider(connector: GlueConnector, tableInfo: Table) extends TimestampProvider {
 
-    val max =
-      for {
-        value <- maxPartitionValue.getValue
-        longValue = FormatParser.parse(value, format)
-        result <- longValue
-      } yield {
-        result
-      }
-
-    AtomicValue(max)
+    override def provide(): Value[Timestamp] = {
+      AtomicValue(Option(connector.getMaxBatchId(tableInfo.table, tableInfo.columns.head)))
+    }
   }
-}
 
-/**
-  * Glue Timestamp Provider
-  *
-  * Provides timestamp metric by table, partition column and appropriate connector
-  */
-class GlueTimestampProvider(connector: GlueConnector, tableInfo: TableColumn) extends TimestampProvider {
+  /**
+    * Current Timestamp Provider
+    *
+    * Provides current time in milliseconds
+    */
+  class CurrentTimestampProvider extends TimestampProvider {
 
-  override def provide(): Value[Timestamp] = {
-    AtomicValue(Option(connector.getMaxBatchId(tableInfo.table, tableInfo.column)))
+    override def provide(): Value[Timestamp] = {
+      AtomicValue(Option(System.currentTimeMillis()))
+    }
   }
-}
 
-/**
-  * Current Timestamp Provider
-  *
-  * Provides current time in milliseconds
-  */
-class CurrentTimestampProvider extends TimestampProvider {
+  /**
+    * Sql Lag Provider
+    *
+    * Provides diff between metrics from "fromProvider" and "toProvider"
+    *
+    * Lag (diff) = fromProvider.metric - toProvider.metric
+    */
+  class SqlLagProvider(fromProvider: TimestampProvider, toProvider: TimestampProvider) extends TimestampProvider {
 
-  override def provide(): Value[Timestamp] = {
-    AtomicValue(Option(System.currentTimeMillis()))
+    override def provide(): Value[Timestamp] = {
+      val lag =
+        for {
+          fromValue <- fromProvider.provide().getValue
+          toValue <- toProvider.provide().getValue
+          lagValue <- Option(fromValue - toValue)
+        } yield lagValue
+
+      AtomicValue(lag)
+    }
   }
-}
 
-/**
-  * Sql Lag Provider
-  *
-  * Provides diff between metrics from "fromProvider" and "toProvider"
-  *
-  * Lag (diff) = fromProvider.metric - toProvider.metric
-  */
-class SqlLagProvider(fromProvider: TimestampProvider, toProvider: TimestampProvider) extends TimestampProvider {
-
-  override def provide(): Value[Timestamp] = {
-    val lag =
-      for {
-        fromValue <- fromProvider.provide().getValue
-        toValue   <- toProvider.provide().getValue
-        lagValue  <- Option(fromValue - toValue)
-      } yield lagValue
-
-    AtomicValue(lag)
-  }
-}
