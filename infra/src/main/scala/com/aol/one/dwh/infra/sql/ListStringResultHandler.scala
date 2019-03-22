9bfc8a10f0612bd0b1e3c438b9555d4171b157d5
maria ra <mariia.rabyk@gmail.com>
2019-03-22 17:17:46 +0200

    Edited handle method in ListStringResultHandler

--- infra/src/main/scala/com/aol/one/dwh/infra/sql/ListStringResultHandler.scala
+++ infra/src/main/scala/com/aol/one/dwh/infra/sql/ListStringResultHandler.scala
@@ -2,17 +2,21 @@ package com.aol.one.dwh.infra.sql
 
 import java.sql.ResultSet
 
+import com.aol.one.dwh.infra.parser.StringToTimestampParser
 import org.apache.commons.dbutils.ResultSetHandler
 
-class ListStringResultHandler(numberOfPartitions: Int) extends ResultSetHandler[Option[List[String]]] {
+class ListStringResultHandler(numberOfPartitions: Int, format: String) extends ResultSetHandler[Option[Long]] {
 
-  override def handle(resultSet: ResultSet): Option[List[String]] = {
+  override def handle(resultSet: ResultSet): Option[Long] = {
 
    val result =  Iterator
     .continually(resultSet.next)
     .takeWhile(identity)
     .map { _ => getColumnValues(numberOfPartitions, resultSet) }.toList
-    Option(result)
+
+    val converted = result.map(value => StringToTimestampParser.parse(value, format)).map(_.getOrElse(0L)).max
+
+    Option(converted)
   }
 
   private def getColumnValues(numberOfPartitions: Int, resultSet: ResultSet): String = {
