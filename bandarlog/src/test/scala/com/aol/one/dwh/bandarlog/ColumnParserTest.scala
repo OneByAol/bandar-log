37e98f45a30b3c54ba46ff50f99e96676c1126d6
maria ra <mariia.rabyk@gmail.com>
2019-04-19 12:59:09 +0300

    Edited test description

--- bandarlog/src/test/scala/com/aol/one/dwh/bandarlog/ColumnParserTest.scala
+++ bandarlog/src/test/scala/com/aol/one/dwh/bandarlog/ColumnParserTest.scala
@@ -8,14 +8,12 @@ class ColumnParserTest extends FunSuite {
 
   val columns = List("year=yyyy", "month=MM", "day=dd")
 
-  test("Parse column and format from bandarlog config") {
+  test("Parse column and its format from bandarlog config") {
 
     val expectedResult = List(Partition("year", "yyyy"), Partition("month", "MM"), Partition("day", "dd"))
     val actualResult = ColumnParser.parseList(columns)
     assert(expectedResult equals actualResult)
 
   }
-
-
 
 }
