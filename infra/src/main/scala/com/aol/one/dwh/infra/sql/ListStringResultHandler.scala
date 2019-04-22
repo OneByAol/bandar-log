288fefadc77c6087b27c370b6be19bae29f284a3
maria ra <mariia.rabyk@gmail.com>
2019-04-22 12:42:35 +0300

    Edited ListStringResultHandler

--- infra/src/main/scala/com/aol/one/dwh/infra/sql/ListStringResultHandler.scala
+++ infra/src/main/scala/com/aol/one/dwh/infra/sql/ListStringResultHandler.scala
@@ -2,24 +2,35 @@ package com.aol.one.dwh.infra.sql
 
 import java.sql.ResultSet
 
+import com.aol.one.dwh.infra.parser.StringToTimestampParser
 import com.aol.one.dwh.infra.util.LogTrait
 import org.apache.commons.dbutils.ResultSetHandler
 
-class ListStringResultHandler(numberOfPartitions: Int) extends ResultSetHandler[Option[List[String]]] with LogTrait {
+class ListStringResultHandler(numberOfPartitions: Int, format: String) extends ResultSetHandler[Option[Long]] with LogTrait {
 
-  override def handle(resultSet: ResultSet): Option[List[String]] = {
+  override def handle(resultSet: ResultSet): Option[Long] = {
 
    val result =  Iterator
     .continually(resultSet.next)
     .takeWhile(identity)
     .map { _ => getColumnValues(numberOfPartitions, resultSet) }.toList
 
-    Option(result)
+    val maxValue = parseValuesToTimestamp(result, format)
+
+    maxValue
   }
 
   private def getColumnValues(numberOfPartitions: Int, resultSet: ResultSet): String = {
-    (1 to numberOfPartitions).map( index => resultSet.getString(index)).toList.mkString(":")
+    (1 to numberOfPartitions)
+      .map( index => resultSet.getString(index))
+      .toList
+      .mkString(":")
+  }
+
+  private def parseValuesToTimestamp(values: List[String], format: String): Option[Long] = {
+    values
+      .map(value => StringToTimestampParser.parse(value, format))
+      .max
   }
 
 }
-
