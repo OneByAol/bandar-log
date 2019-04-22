9e136935cd7953b345ca30f242419b29193413dc
maria ra <mariia.rabyk@gmail.com>
2019-04-22 17:02:31 +0300

    Edited object name

--- bandarlog/src/main/scala/com/aol/one/dwh/bandarlog/providers/ProviderFactory.scala
+++ bandarlog/src/main/scala/com/aol/one/dwh/bandarlog/providers/ProviderFactory.scala
@@ -11,7 +11,7 @@ package com.aol.one.dwh.bandarlog.providers
 import com.aol.one.dwh.bandarlog.connectors.{GlueConnector, JdbcConnector}
 import com.aol.one.dwh.bandarlog.providers.SqlProvider.TimestampProvider
 import com.aol.one.dwh.infra.config.{ConnectorConfig, Table}
-import com.aol.one.dwh.infra.sql.MaxValuesQuery
+import com.aol.one.dwh.infra.sql.ValuesQuery
 import com.aol.one.dwh.infra.sql.pool.SqlSource.{GLUE, PRESTO, VERTICA}
 import com.typesafe.config.Config
 import com.aol.one.dwh.infra.config.RichConfig._
@@ -23,9 +23,9 @@ class ProviderFactory(mainConfig: Config, connectionPoolHolder: ConnectionPoolHo
     connector.connectorType match {
 
       case VERTICA | PRESTO => {
-        val query = MaxValuesQuery.get(connector.connectorType)(table)
+        val query = ValuesQuery.get(connector.connectorType, table)
         val connectionPool = connectionPoolHolder.get(connector)
-        new SqlTimestampProvider(JdbcConnector(connector.connectorType, connectionPool), query, table)
+        new SqlTimestampProvider(JdbcConnector(connector.connectorType, connectionPool), query)
       }
 
       case GLUE => {
