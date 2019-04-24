b2244b358c27dea91b465ff9af477742aad13a73
maria ra <mariia.rabyk@gmail.com>
2019-04-24 16:12:40 +0300

    Refactored ColumnParser

--- infra/src/main/scala/com/aol/one/dwh/infra/parser/ColumnParser.scala
+++ infra/src/main/scala/com/aol/one/dwh/infra/parser/ColumnParser.scala
@@ -1,22 +1,20 @@
 package com.aol.one.dwh.infra.parser
 
-import com.aol.one.dwh.infra.config._
 import scala.util.parsing.combinator.RegexParsers
-
 
 object ColumnParser extends RegexParsers {
 
   override def skipWhitespace: Boolean = false
 
-  def pairSep: Parser[String] = "=".r
-  def column: Parser[String] = "\\w+".r
-  def format: Parser[String] = "[\\w\\-\\.\\s\\':,]+".r
+  def pairSeparator: Parser[String] = "=".r
+  def columnName: Parser[String] = "\\w+".r
+  def columnFormat: Parser[String] = "[\\w\\-\\.\\s\\':,]+".r
 
-  def pair: Parser[DatetimePatition] = column ~ pairSep ~ format ^^ {
-    case c ~ _ ~ f => DatetimePatition(c, f)
+  def pair: Parser[(String, String)] = columnName ~ pairSeparator ~ columnFormat ^^ {
+    case c ~ _ ~ f => (c, f)
   }
 
-  def parse(in: String): DatetimePatition = {
+  def parse(in: String): (String, String) = {
     parseAll(pair, in) match {
       case Success(result, _) => result
       case NoSuccess(msg, next) =>
@@ -24,7 +22,7 @@ object ColumnParser extends RegexParsers {
     }
   }
 
-  def parseList(in: List[String]): List[DatetimePatition] = {
+  def parseList(in: List[String]): List[(String, String)] = {
     in.map(parse)
   }
 }
