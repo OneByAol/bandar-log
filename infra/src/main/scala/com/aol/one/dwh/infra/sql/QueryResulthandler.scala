1120ad719e333fff0d70d2c63e383bc5fb573d33
maria ra <mariia.rabyk@gmail.com>
2019-04-25 11:47:25 +0300

    Initial commit

---
+++ infra/src/main/scala/com/aol/one/dwh/infra/sql/QueryResulthandler.scala
@@ -0,0 +1,29 @@
+package com.aol.one.dwh.infra.sql
+
+import org.apache.commons.dbutils.ResultSetHandler
+
+object QueryResulthandler {
+
+  def get(query: Query): ResultSetHandler[Option[Long]] = query match {
+      case VerticaMaxValuesQuery(table) =>
+        if (table.formats.isDefined) {
+          val parititions = table.columns
+          val numberOfColumns = parititions.length
+          val format = table.formats.get.mkString(":")
+          new ListStringResultHandler(numberOfColumns, format)
+        } else {
+          new LongValueResultHandler
+        }
+
+      case PrestoMaxValuesQuery(table) =>
+        if (table.formats.isDefined) {
+          val parititions = table.columns
+          val numberOfColumns = parititions.length
+          val format = table.formats.get.mkString(":")
+          new ListStringResultHandler(numberOfColumns, format)
+        } else {
+          new LongValueResultHandler
+        }
+    }
+
+}
