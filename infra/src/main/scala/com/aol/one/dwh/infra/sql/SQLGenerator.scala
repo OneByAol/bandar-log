2789344b34b40de3ea5c48fadb6d6677a2b1c0d6
maria ra <mariia.rabyk@gmail.com>
2019-04-30 12:41:09 +0300

    Renamed class and refactored

--- infra/src/main/scala/com/aol/one/dwh/infra/sql/SQLGenerator.scala
+++ infra/src/main/scala/com/aol/one/dwh/infra/sql/SQLGenerator.scala
@@ -1,17 +1,17 @@
 package com.aol.one.dwh.infra.sql
 
-import com.aol.one.dwh.infra.config.TableColumn
+import com.aol.one.dwh.infra.config.Table
 
 object SQLGenerator {
 
-  def generateSql(table: TableColumn): String = {
-    if (table.formats.isDefined) {
-      val columns = table.columns.mkString(", ")
-      s"SELECT DISTINCT $columns FROM ${table.table}"
-    } else {
-      val column = table.columns.head
-      s"SELECT MAX($column) AS $column FROM ${table.table}"
+  def generateSql(table: Table): String = {
+    table.formats match {
+      case Some(_) =>
+        val columns = table.columns.mkString(", ")
+        s"SELECT DISTINCT $columns FROM ${table.table}"
+      case None =>
+        val column = table.columns.head
+        s"SELECT MAX($column) AS $column FROM ${table.table}"
     }
   }
-
 }
