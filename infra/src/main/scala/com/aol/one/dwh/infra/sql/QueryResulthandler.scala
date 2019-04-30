389ed4e7a094579675f4c59c2411489c324a059d
maria ra <mariia.rabyk@gmail.com>
2019-04-30 12:42:44 +0300

    Renamed class and refactored

--- infra/src/main/scala/com/aol/one/dwh/infra/sql/QueryResulthandler.scala
+++ infra/src/main/scala/com/aol/one/dwh/infra/sql/QueryResulthandler.scala
@@ -1,28 +1,24 @@
 package com.aol.one.dwh.infra.sql
 
+import com.aol.one.dwh.infra.config.Table
 import org.apache.commons.dbutils.ResultSetHandler
 
 object QueryResulthandler {
 
   def get(query: Query): ResultSetHandler[Option[Long]] = query match {
-      case VerticaMaxValuesQuery(table) =>
-        if (table.formats.isDefined) {
-          val parititions = table.columns
-          val numberOfColumns = parititions.length
-          val format = table.formats.get.mkString(":")
-          new ListStringResultHandler(numberOfColumns, format)
-        } else {
-          new LongValueResultHandler
-        }
-
-      case PrestoMaxValuesQuery(table) =>
-        if (table.formats.isDefined) {
-          val parititions = table.columns
-          val numberOfColumns = parititions.length
-          val format = table.formats.get.mkString(":")
-          new ListStringResultHandler(numberOfColumns, format)
-        } else {
-          new LongValueResultHandler
-        }
+      case VerticaMaxValuesQuery(table) => getHandler(table)
+      case PrestoMaxValuesQuery(table)  => getHandler(table)
     }
+
+  private def getHandler(table: Table): ResultSetHandler[Option[Long]] = {
+    table
+      .formats
+      .map { format =>
+        val parititions = table.columns
+        val numberOfColumns = parititions.length
+        new ListStringResultHandler(numberOfColumns, format.mkString(":"))
+      }
+      .getOrElse(new LongValueResultHandler)
+  }
+
 }
